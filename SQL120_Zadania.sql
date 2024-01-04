4f525d7a2a845044f9fe1452d2654732ace5a975
ŁukasikT <100628114+LukasikT@users.noreply.github.com>
2024-01-04 19:56:23 +0100

    Add files via upload

---
+++ SQL120_Zadania.sql
@@ -0,0 +1,33 @@
+
+ZADANIA
+
+USE AdventureWorks2014
+GO
+
+-- 1. Wyœwietl wszystkie produkty (tabela Production.Product) koloru czarnego,
+-- posortowane malej¹co wg ceny (ListPrice) 
+
+-- 2. Wyœwietl wszystkie produkty koloru czerwonego i niebieskiego, których nazwa zaczyna siê 
+-- na 'L', posortowane wg rozmiaru malej¹co i ceny rosn¹co
+
+-- 3. Wyœwietl wiersze z tabeli Sales.SalesTerritory, przypisane do Europy, posortowane wg nazwy kraju
+-- podpowiedŸ: Group jest s³owem zastrzezonym w SQL Server, 
+--   pos³uguj¹c siê kolumn¹ o tej nazwie w zapytaniach powinieneœ wiêc u¿ywaæ nawiasow kwadratowych
+
+-- 4. Wyœwietl wiersze z tabeli Sales.SalesTerritory, 
+-- posortowane wg grupy regionów (kontynentów) malej¹co i nazwy kraju rosn¹co
+
+-- 5. Wyœwietl zamówienia przypisane do obszarów o ID 7,8,9
+
+-- 6. Wyœwietl 10 ostatnich zamówieñ,
+-- przypisanych do obszarów o ID 7,8,9 o 
+-- wartoœci (kolumna SubTotal) mniejszej ni¿ 100
+
+-- 7. Wyœwietl 10 zamówieñ na najwy¿sz¹ kwotê, przypisanych do obszaru 7,
+
+-- 8. Wyœwietl zamówienia przypisanych do obszaru 7, 
+-- bez przypisanego numeru kardy kredytowej (kolumna CreditCardID)
+
+-- 9. Wyœwietl zamówienia przypisanych do obszaru 7, z przypisanym numerem kardy kredytowej 
+
+-- 10. Wyœwietl zamówienia z roku 2011, posortuj wg daty zamówienia
